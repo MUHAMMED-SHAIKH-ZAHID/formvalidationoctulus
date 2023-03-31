c3336b9b4db7b9b3765fefd208aaa72559ed2c35
MUHAMMED-SHAIKH-ZAHID <muhdshaikhzahid@gmail.com>
2023-03-31 20:17:35 +0530

    completed-all

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 
 import { BrowserRouter } from 'react-router-dom';
 import './App.css';
-import Signup from './components/Signup';
 import RoutePage from './Route/RoutePage';
 
 function App() {
--- src/components/Jobposition.jsx
+++ src/components/Jobposition.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { BiArrowBack } from "react-icons/bi";
-import { Link, useNavigate } from "react-router-dom";
+import {  useNavigate } from "react-router-dom";
 import { UpdateData } from "./api/services/FormRequest";
 
 const Jobposition = ({setForm ,id}) => {
